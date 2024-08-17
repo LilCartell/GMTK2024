9e5952ab2d0b167581595f4b393854115843ba83
LilCartell <lilian.cartellier@gmail.com>
2024-08-17 11:08:39 +0200

    Added the few still useful scripts from last project

---
+++ Assets/Scripts/Directions.cs
@@ -0,0 +1,7 @@
+﻿public enum Directions
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
---
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	public AudioSource MusicsSource;
+	public AudioSource SoundEffectsSource;
+
+	public AudioClip TitleScreenMusic;
+	public AudioClip IntroductionMusic;
+	public AudioClip LevelsMusic;
+	public AudioClip EndMusic;
+
+	public AudioClip SwapSound;
+	public AudioClip RockPushSound;
+	public AudioClip RockFallingSound;
+	public AudioClip PushyFallingSound;
+	public AudioClip ReachedDoorSound;
+
+	public static SoundManager Instance { get; private set; }
+
+	private void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+
+		Instance = this;
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	public void PlayMusic(AudioClip musicClip)
+	{
+		if(musicClip != null && musicClip != MusicsSource.clip)
+        {
+			MusicsSource.clip = musicClip;
+			MusicsSource.Play();
+		}
+	}
+
+	public void PlaySound(AudioClip effectSound)
+	{
+		if(effectSound)
+        {
+			SoundEffectsSource.clip = effectSound;
+			SoundEffectsSource.Play();
+		}
+	}
+}
