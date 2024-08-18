812c9292bc6089014f412d49cd5f9f338cc4d02c
LilCartell <lilian.cartellier@gmail.com>
2024-08-18 23:10:39 +0200

    Added rotation ability to automatic guns

--- Assets/Scripts/Shmup/AutomaticGun.cs
+++ Assets/Scripts/Shmup/AutomaticGun.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class AutomaticGun : Gun
 {
     public float firstShotOffset = 0.0f;
+    public float rotationAmplitude = 0.0f;
+    public float rotationSpeed = 0.0f;
 
     private bool _firstShotIsDone = false;
     private float _timeBeforeAllowFirstShot;
+    private bool _isRotationIncreasing;
+    private float _currentRotationDegree;
 
     private void Awake()
     {
@@ -29,6 +33,29 @@ public class AutomaticGun : Gun
         else
         {
             RequestShoot();
+        }
+
+        if(rotationSpeed > 0.0f &&  rotationAmplitude > 0.0f)
+        {
+            if(_isRotationIncreasing)
+            {
+                _currentRotationDegree +=  rotationSpeed * Time.deltaTime;
+                if(_currentRotationDegree >= rotationAmplitude)
+                {
+                    _currentRotationDegree = rotationAmplitude;
+                    _isRotationIncreasing = false;
+                }
+            }
+            else
+            {
+                _currentRotationDegree -=  rotationSpeed * Time.deltaTime;
+                if(_currentRotationDegree <= -rotationAmplitude)
+                {
+                    _currentRotationDegree = -rotationAmplitude;
+                    _isRotationIncreasing = true;
+                }
+            }
+            this.transform.localRotation = Quaternion.Euler(0, 0, _currentRotationDegree);
         }
     }
 }
