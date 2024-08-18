66ce5f2bcb65aa3159e1a7c9571edf0d270510ab
LilCartell <lilian.cartellier@gmail.com>
2024-08-18 20:23:49 +0200

    Fix money siphoning bug

--- Assets/Scripts/ShipBuilding/ActionPattern/PlaceSpecInCellsShipBuildingAction.cs
+++ Assets/Scripts/ShipBuilding/ActionPattern/PlaceSpecInCellsShipBuildingAction.cs
@@ -1,5 +1,6 @@
 public class PlaceSpecInCellsShipBuildingAction : IShipBuildingAction
 {
+    private ShipPartSpecification _oldSpecsInCell;
     private ShipPartSpecification _shipPartSpecification;
     private ShipBuildingCell _shipBuildingCell;
 
@@ -10,13 +11,23 @@ public class PlaceSpecInCellsShipBuildingAction : IShipBuildingAction
     }
     public void Do()
     {
+        _oldSpecsInCell = _shipBuildingCell.LoadedSpecification;
         _shipBuildingCell.PlaceSpecInCell(_shipPartSpecification);
         GameSession.Instance.CurrentMoney -= _shipPartSpecification.ShipPartArchetype.Cost;
+        if(_oldSpecsInCell != null)
+        {
+            GameSession.Instance.CurrentMoney += _oldSpecsInCell.ShipPartArchetype.Cost;
+        }
     }
 
     public void Undo()
     {
         _shipBuildingCell.RemoveSpecFromCell();
         GameSession.Instance.CurrentMoney += _shipPartSpecification.ShipPartArchetype.Cost;
+        if(_oldSpecsInCell != null)
+        {
+            _shipBuildingCell.PlaceSpecInCell(_oldSpecsInCell);
+            GameSession.Instance.CurrentMoney -= _oldSpecsInCell.ShipPartArchetype.Cost;
+        }
     }
 }
