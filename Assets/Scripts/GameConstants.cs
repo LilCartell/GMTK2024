983bb63c97f9e6d22be3c5c12ec4a230307e83b8
Flurriste <chaillot.m@gmail.com>
2024-08-19 20:31:54 +0200

    push

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -1,5 +1,5 @@
 public static class GameConstants
 {
-    public const float SHMUP_PLAYER_SPEED_FACTOR = 50.0f;
+    public const float SHMUP_PLAYER_SPEED_FACTOR = 75.0f;
     public const float SHIP_PARTS_SIZE = 0.6f;
 }
